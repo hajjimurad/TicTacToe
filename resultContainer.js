436c027c4b040fb1878c25e41939bdabe61a16c0
murad <hajjimurad@gmail.com>
2015-04-08 23:54:19 +0300

    clean up

--- resultContainer.js
+++ resultContainer.js
@@ -1,27 +1,27 @@
+if(result != null) {
+    resultCounter.total++;
+    //---
+}
+switch(result) {
+    case "X":
+        resultCounter.resX++;
+        break;
+    case "0":
+        resultCounter.res0++;
+        break;
+    case "-":
+        resultCounter.resNone++;
+        break;
+    default:
+        throw "unexpected: " + result;
+        break;
+};
 //--- counter for results
+//--- checks and counts results
+this.countResult = function(result) {
+}
 function ResultsCounter() {
     var resultCounter = { resX: 0, res0: 0, resNone: 0, total: 0};
-    //--- checks and counts results
-    this.countResult = function(result) {
-        if(result != null) {
-            resultCounter.total++;
-            //---
-            switch(result) {
-                case "X":
-                    resultCounter.resX++;
-                    break;
-                case "0":
-                    resultCounter.res0++;
-                    break;
-                case "-":
-                    resultCounter.resNone++;
-                    break;
-                default:
-                    throw "unexpected: " + result;
-                    break;
-            }
-        }
-    };
     //--- gets results
     this.getResults = function() {
         return resultCounter;
